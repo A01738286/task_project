90259abd63f51468eae492049f526b7b7643a29a
A01738286 <a01738286@tec.mx>
2023-11-07 16:52:00 -0600

    feat: add menu

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-#CREATE TASK LIST
+# CREATE TASK LIST
 
-#ADD FUNCTIONS FOR TASK MANAGEMENT 
+# ADD FUNCTIONS FOR TASK MANAGEMENT 
+def print_menu():
+    print("Main menu")
+    print("1. Add tasks")
+    print("2. Complete task")
+    print("3. Show task list")
+    print("\n")
 
-#CREATE MAIN FUNCTION
+# CREATE MAIN FUNCTION
+def main():
+    print_menu()
 
-#CALL MAIN FUCNTION 
+# CALL MAIN FUNCTION
+if __name__ == "__main__":
+    main()
